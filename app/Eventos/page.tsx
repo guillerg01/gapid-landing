c2c4249c4013eb55f120254014d5ca22b30a3837
cynthiabg <cynthiabg@estudiantes.uci.cu>
2024-08-17 14:09:42 -0500

    eventos1

---
+++ app/Eventos/page.tsx
@@ -0,0 +1,198 @@
+import Header from "../Componentes/header";
+
+export default function Eventos() {
+  return (
+    <div>
+      <Header></Header>
+
+      <div className="flex flex-col w-full   mt-20 gap-10 ">
+        <div className="flex flex-row  gap-10 pl-48 pr-28">
+          <div className="flex flex-col gap-4 py-32 justify-center   ">
+            <div className="text-[#00221C] font-roboto  text-base ">
+              FEATURED CUSTOMERS{" "}
+            </div>
+            <div className="font-roboto text-[#00221C] text-3xl pr-10 font-semibold">
+              Very precise documentation with many features well explained.
+            </div>
+            <div className="font-roboto text-[#00221C] text-base ">
+              “ Now that we’re using Space, everyone involved in a project has
+              visibility into things like changes in scope and other status
+              updates. “
+            </div>
+            <div className="flex flex-col gap-0">
+              {" "}
+              <div className="mt-4 font-roboto font-semibold text-lg">
+                Andrea
+              </div>
+              <div className=" text-[#677788] font-roboto text-sm ">
+                Director of Sales | MailChimp
+              </div>
+            </div>
+          </div>
+          <div className="h-full w-full flex ">
+            <img src="./Col.png"></img>
+          </div>
+        </div>
+
+        <div className="  grid  grid-cols-3 gap-7 pl-48 pr-44  ">
+          <div className="flex flex-col gap-4 pb-5">
+            <img src="./Imagen.png"></img>
+            <div className=" font-roboto text-[#00221C] text-xl font-semibold">
+              Capsule scales at speed with Space
+            </div>
+            <div className="text-[#223B41] text-base font-roboto">
+              Capsule has turned payments into a competitive advantage working
+              with Space, allowing it to scale at speed across multiple markets.
+            </div>
+            <div className="flex flex-row gap-3   items-center">
+              <div className="text-[#008060] font-semibold">Leer más </div>
+              <img src="./Path (Stroke) (2).svg"></img>{" "}
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 pb-5">
+            <img src="./Imagen.png"></img>
+            <div className=" font-roboto text-[#00221C] text-xl font-semibold">
+              Capsule scales at speed with Space
+            </div>
+            <div className="text-[#223B41] text-base font-roboto">
+              Capsule has turned payments into a competitive advantage working
+              with Space, allowing it to scale at speed across multiple markets.
+            </div>
+            <div className="flex flex-row gap-3   items-center">
+              <div className="text-[#008060] font-semibold">Leer más </div>
+              <img src="./Path (Stroke) (2).svg"></img>{" "}
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 pb-5">
+            <img src="./Imagen.png"></img>
+            <div className=" font-roboto text-[#00221C] text-xl font-semibold">
+              Capsule scales at speed with Space
+            </div>
+            <div className="text-[#223B41] text-base font-roboto">
+              Capsule has turned payments into a competitive advantage working
+              with Space, allowing it to scale at speed across multiple markets.
+            </div>
+            <div className="flex flex-row gap-3   items-center">
+              <div className="text-[#008060] font-semibold">Leer más </div>
+              <img src="./Path (Stroke) (2).svg"></img>{" "}
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 pb-5">
+            <img src="./Imagen.png"></img>
+            <div className=" font-roboto text-[#00221C] text-xl font-semibold">
+              Capsule scales at speed with Space
+            </div>
+            <div className="text-[#223B41] text-base font-roboto">
+              Capsule has turned payments into a competitive advantage working
+              with Space, allowing it to scale at speed across multiple markets.
+            </div>
+            <div className="flex flex-row gap-3   items-center">
+              <div className="text-[#008060] font-semibold">Leer más </div>
+              <img src="./Path (Stroke) (2).svg"></img>{" "}
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 pb-5">
+            <img src="./Imagen.png"></img>
+            <div className=" font-roboto text-[#00221C] text-xl font-semibold">
+              Capsule scales at speed with Space
+            </div>
+            <div className="text-[#223B41] text-base font-roboto">
+              Capsule has turned payments into a competitive advantage working
+              with Space, allowing it to scale at speed across multiple markets.
+            </div>
+            <div className="flex flex-row gap-3   items-center">
+              <div className="text-[#008060] font-semibold">Leer más </div>
+              <img src="./Path (Stroke) (2).svg"></img>{" "}
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 pb-5">
+            <img src="./Imagen.png"></img>
+            <div className=" font-roboto text-[#00221C] text-xl font-semibold">
+              Capsule scales at speed with Space
+            </div>
+            <div className="text-[#223B41] text-base font-roboto">
+              Capsule has turned payments into a competitive advantage working
+              with Space, allowing it to scale at speed across multiple markets.
+            </div>
+            <div className="flex flex-row gap-3   items-center">
+              <div className="text-[#008060] font-semibold">Leer más </div>
+              <img src="./Path (Stroke) (2).svg"></img>{" "}
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 pb-5">
+            <img src="./Imagen.png"></img>
+            <div className=" font-roboto text-[#00221C] text-xl font-semibold">
+              Capsule scales at speed with Space
+            </div>
+            <div className="text-[#223B41] text-base font-roboto">
+              Capsule has turned payments into a competitive advantage working
+              with Space, allowing it to scale at speed across multiple markets.
+            </div>
+            <div className="flex flex-row gap-3   items-center">
+              <div className="text-[#008060] font-semibold">Leer más </div>
+              <img src="./Path (Stroke) (2).svg"></img>{" "}
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 pb-5">
+            <img src="./Imagen.png"></img>
+            <div className=" font-roboto text-[#00221C] text-xl font-semibold">
+              Capsule scales at speed with Space
+            </div>
+            <div className="text-[#223B41] text-base font-roboto">
+              Capsule has turned payments into a competitive advantage working
+              with Space, allowing it to scale at speed across multiple markets.
+            </div>
+            <div className="flex flex-row gap-3   items-center">
+              <div className="text-[#008060] font-semibold">Leer más </div>
+              <img src="./Path (Stroke) (2).svg"></img>{" "}
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 pb-5">
+            <img src="./Imagen.png"></img>
+            <div className=" font-roboto text-[#00221C] text-xl font-semibold">
+              Capsule scales at speed with Space
+            </div>
+            <div className="text-[#223B41] text-base font-roboto">
+              Capsule has turned payments into a competitive advantage working
+              with Space, allowing it to scale at speed across multiple markets.
+            </div>
+            <div className="flex flex-row gap-3   items-center">
+              <div className="text-[#008060] font-semibold">Leer más </div>
+              <img src="./Path (Stroke) (2).svg"></img>{" "}
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#00BD8E1A] w-full flex flex-row gap-44 items-center justify-center  py-24 px-48">
+          <div className=" flex flex-col max-w-[30rem]">
+            <div className="text-[#00221C] font-semibold font-roboto text-2xl flex">
+              Suscríbete a nuestro boletín
+            </div>
+            <div className="text-[#51596C] text-base font-roboto flex">
+              Manténgase actualizado sobre nuevos lanzamientos y funciones,
+              guías y actualizaciones de la comunidad.
+            </div>
+          </div>
+
+          <div className="relative flex flex-col gap-5">
+            <input
+              placeholder="Your business email"
+              className="bg-#FFFFFF py-5 pl-3 pr-36 rounded-md placeholder:text-[#97A4AF] placeholder:font-roboto shadow-sm"
+            ></input>
+            <div className="absolute top-3 right-4 bg-[#008060] text-[#FFFFFF] flex  px-2 py-2 rounded-lg ">
+              Subscribirse
+            </div>
+            <div className="flex flex-row gap-3 items-center">
+              <div className="text-[#008060]  font-roboto text-sm">
+                Create a free account
+              </div>
+              <div>
+                <img src="./Path (Stroke) (2).svg"></img>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
